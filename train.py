ce3b8cad4bcdc829d4a5bd011d69798c8a748e14
dorsimantov <dorsimantov@mail.tau.ac.il>
2024-10-30 17:23:44 +0200

    update

--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 # TODO: Read task hyperparams from config/train/TRAIN_NAME/train_info.yaml into a dictionary
 # TODO: Read model hyperparams (for specific task) from config/train/TRAIN_NAME/MODEL_NAME.yaml into a dictionary
 # TODO: Train the model
-#  Dump training weights in models/weights/TRAIN_NAME, unless we disable weights saving (in base config, task info or model config for task??? idk)
+#  Dump training weights in models/weights/TRAIN_NAME, unless we disable weights saving (in either base config, task info or model config for task)
+#  Dump training logs in models/weights/TRAIN_NAME/logs
 # TODO: Save task hyperparams from config/train/TRAIN_NAME/train_info.yaml and current time as train_info.json in both results/train/TRAIN_NAME and models/MODEL_NAME/weights/TRAIN_NAME
 # TODO: Save model hyperparams from config/train/TRAIN_NAME/MODEL_NAME.yaml and current time as MODEL_NAME.json in both results/train/TRAIN_NAME/MODEL_NAME and models/MODEL_NAME/weights/TRAIN_NAME
 # TODO: Dump results in the results/train/TRAIN_NAME/MODEL_NAME (separate folders for outputs, plots, and metrics)
